c03bb6df80ce07e7ffa7cd2d85b189bac2433e6f
Acer <Acer@DESKTOP-5LU3HEA>
2023-09-17 19:33:15 +0300

    Осталось решить проблему с подозрениями на бота и добавлением нулей перед именем файла

--- python_lab_1.py
+++ python_lab_1.py
@@ -1,26 +1,29 @@
 from bs4 import BeautifulSoup
-
+from time import sleep
 import os
 
 import requests
 
-films = ["https://www.kinopoisk.ru/series/1032606/"]
-header = {
-    "User-Agent":"Safari/537.36"
-}
+films = ["https://www.kinopoisk.ru/film/1032606/reviews/ord/rating/status/all/perpage/200/", "https://www.kinopoisk.ru/film/4484/reviews/ord/date/status/all/perpage/200/",
+         "https://www.kinopoisk.ru/film/915196/reviews/ord/date/status/all/perpage/200/", "https://www.kinopoisk.ru/film/495892/reviews/ord/date/status/all/perpage/200/",
+         "https://www.kinopoisk.ru/film/404366/reviews/ord/date/status/all/perpage/200/"]
+header = {"User-Agent":"YaBrowser/23.7.5.734 Yowser/2.5 Safari/537.36"}
+
 for film in films:
     good_comment_num = 1
     bad_comment_num = 1
-    film_page = requests.get(film, headers=header).content
-    soup = BeautifulSoup(film_page, "lxml")
-    for comment in soup.find_all("section", class_ = "styles_root__644Yf styles_review__GN2Uy styles_rootPositive__sl4cN"):
-        with open(f"C:/python_lab1/dataset/good/{good_comment_num}", "w", encoding = "utf-8") as file:
+    soup = BeautifulSoup(requests.get(film, headers=header).content, "lxml")
+    print(soup.text)
+    for comment in soup.find_all("div", class_ = "response good"):
+        with open(f"dataset/good/0{good_comment_num}", "w", encoding = "utf-8") as file:
             file.write(comment.text)
             good_comment_num+=1
-    for comment in soup.find_all("section", class_ = "styles_root__644Yf styles_review__GN2Uy styles_rootNegative___OV9G"):
-        with open(f"C:/python_lab1/dataset/bad/{bad_comment_num}", "w", encoding = "utf-8") as file:
+    for comment in soup.find_all("div", class_ = "response bad"):
+        with open(f"dataset/bad/0{bad_comment_num}", "w", encoding = "utf-8") as file:
             file.write(comment.text)
             bad_comment_num+=1
+    sleep(5)
+
                 
         
 
