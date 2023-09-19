bf8a2c4ff5f0b41787fdb9693d8ca3a7b4a8cf26
Acer <Acer@DESKTOP-5LU3HEA>
2023-09-19 09:28:22 +0300

    Обход проверки на бота нужно еще немного улучшить, добавить фильмы

--- python_lab_1.py
+++ python_lab_1.py
@@ -12,22 +12,26 @@ header = {"User-Agent":UserAgent().random,
           "cookie":"L=fQ5KeUJhVV99WnVScXNCDw5Wc353CWN+ChYkKHB1dkU=.1676924953.15259.367197.77"}
 good_comment_num = 1
 bad_comment_num = 1
-i = 1
+film_count = 1
 session = requests.Session()
 session.auth = ('user', 'pass')
 for film in films:
     soup = BeautifulSoup(session.get(film, headers=header).content, "lxml")
-    print(soup.text)
+    film_name = BeautifulSoup(str(soup.find("a", class_ = "breadcrumbs__link")), "lxml").text
     for comment in soup.find_all("div", class_ = "response good"):
+        comment = BeautifulSoup(str(comment), "lxml").findChild("span", class_="_reachbanner_")
         with open(f"dataset/good/0{good_comment_num}", "w", encoding = "utf-8") as file:
-            file.write(comment.text)
+            file.write(film_name)
+            file.write(BeautifulSoup(str(comment), "lxml").text)
             good_comment_num+=1
     for comment in soup.find_all("div", class_ = "response bad"):
+        comment = BeautifulSoup(str(comment), "lxml").findChild("span", class_="_reachbanner_")
         with open(f"dataset/bad/0{bad_comment_num}", "w", encoding = "utf-8") as file:
-            file.write(comment.text)
+            file.write(film_name)
+            file.write(BeautifulSoup(str(comment), "lxml").text)
             bad_comment_num+=1
-    sleep(20*i/1.1 - 0.2)
-    i+=1
+    sleep(20*film_count/1.1 - 0.2)
+    film_count+=1
 
                 
         
